7fd683fd19bfd8d5f1d94f0832fba0fa15e31828
Bra-Moch <quarm33@gmail.com>
2023-04-25 15:59:42 -0700

    updated converter

--- converter.c
+++ converter.c
@@ -7,17 +7,21 @@
  * @lowercase: flag if hexa values need to be lowercase
  * Return: result string
  */
+
 char *convert(unsigned long int num, int base, int lowercase)
 {
+	/** Static variables for base chtrs and resulting string buffer */
 	static char *rep;
 	static char buffer[50];
 	char *ptr;
 
+	/** Set hexadecimal characters based on lowercase flag */
 	rep = (lowercase)
 		? "0123456789abcdef"
 		: "0123456789ABCDEF";
 	ptr = &buffer[49];
 	*ptr = '\0';
+	/** perform the conversion */
 	do {
 		*--ptr = rep[num % base];
 		num /= base;
