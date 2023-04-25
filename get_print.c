d4e5661a827a87d2bd43cb284ee4995f9f24e727
Bravo10 <bmwinyoglee@gmail.com>
2023-04-25 18:41:58 +0000

    commit changes

--- get_print.c
+++ get_print.c
@@ -2,10 +2,10 @@
 
 /**
  * get_print - selects the right printing function
- * depending on the conversion specifier passed to qb_printf
+ * depending on the conversion specifier passed to _printf
  * @s: character that holds the conversion specifier
  * Description: the function loops through the structs array
- * func_arr[] to find a match between the specifier passed to qb_printf
+ * func_arr[] to find a match between the specifier passed to _printf
  * and the first element of the struct, and then the approriate
  * printing function
  * Return: a pointer to the matching printing function
